38de7b1dc127f639e91cae3f68a5343d25f4bdd0
YOONSOO7429 <125416958+YOONSOO7429@users.noreply.github.com>
2024-08-09 16:44:27 +0900

    test: users 테스트 코드 작성

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,18 +1,139 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
+import { AuthService } from 'src/auth/auth.service';
+import { KakaoAuthGuard } from 'src/auth/guard/kakao.guard';
+import { Request, Response } from 'express';
+import { MockUsersRepository } from './users.service.spec';
+import { Users } from './entity/users.entity';
+import { UserType } from './enum/userType.enum';
+
+class MockKakaoAuthGuard {
+  canActivate = jest.fn().mockReturnValue(true);
+}
+
+class MockUsersService {
+  findUserByEmail = jest.fn();
+  createUser = jest.fn();
+}
+
+class MockAuthService {
+  validateUser = jest.fn();
+  getToken = jest.fn();
+}
+
+const mockUser = new MockUsersRepository().mockUser;
 
 describe('UsersController', () => {
-  let controller: UsersController;
+  let usersController: UsersController;
+  let usersService: UsersService;
+  let authService: AuthService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
-    }).compile();
+      providers: [
+        { provide: UsersService, useClass: MockUsersService },
+        { provide: AuthService, useClass: MockAuthService },
+      ],
+    })
+      .overrideGuard(KakaoAuthGuard)
+      .useClass(MockKakaoAuthGuard)
+      .compile();
 
-    controller = module.get<UsersController>(UsersController);
+    usersController = module.get<UsersController>(UsersController);
+    usersService = module.get<UsersService>(UsersService);
+    authService = module.get<AuthService>(AuthService);
   });
 
   it('should be defined', () => {
-    expect(controller).toBeDefined();
+    expect(usersController).toBeDefined();
+  });
+
+  describe('kakaoCallback', () => {
+    it('user가 존재하면 token을 발생시키고...', async () => {
+      const req = {
+        user: {
+          provider: 'kakao',
+          name: '홍길동',
+          _json: {
+            kakao_account: {
+              email: 'test@example.com',
+              birthyear: '1995',
+              birthday: '0913',
+              phone_number: '+82 10-1234-5678',
+            },
+            properties: {
+              profile_image: null,
+            },
+          },
+        },
+      } as unknown as Request;
+
+      const res = {} as Response;
+
+      jest.spyOn(authService, 'validateUser').mockResolvedValue(mockUser);
+      jest.spyOn(authService, 'getToken').mockResolvedValue('mock-token');
+
+      await usersController.kakaoCallback(req, res);
+
+      expect(authService.validateUser).toHaveBeenCalledWith(
+        'test@example.com',
+        'kakao',
+      );
+      expect(authService.getToken).toHaveBeenCalledWith(mockUser.userId);
+    });
+
+    it('user가 없을 경우 newUser를 생성 후에 token 발생', async () => {
+      const req = {
+        user: {
+          provider: 'kakao',
+          _json: {
+            kakao_account: {
+              email: 'new@daum.net',
+              name: '홍길순',
+              birthyear: '1995',
+              birthday: '0913',
+              phone_number: '+82 10-9876-5432',
+            },
+            properties: {
+              profile_image: 'newImage.png',
+            },
+          },
+        },
+      } as unknown as Request;
+
+      const res = {} as Response;
+
+      const newUserDto = {
+        email: 'new@daum.net',
+        provider: 'kakao',
+        name: '홍길순',
+        profileImage: 'newImage.png',
+        phoneNumber: '010-9876-5432',
+        birth: '1995.09.13',
+      };
+
+      const newUser: Users = {
+        ...newUserDto,
+        userId: 3,
+        userType: UserType.Customer,
+        userCreatedAt: new Date(),
+        userUpdatedAt: new Date(),
+      };
+
+      jest.spyOn(authService, 'validateUser').mockResolvedValue(undefined);
+      jest.spyOn(usersService, 'createUser').mockResolvedValue(newUser);
+      jest.spyOn(authService, 'getToken').mockResolvedValue('mock-new-token');
+
+      await usersController.kakaoCallback(req, res);
+
+      expect(authService.validateUser).toHaveBeenCalledWith(
+        'new@daum.net',
+        'kakao',
+      );
+      expect(usersService.createUser).toHaveBeenCalledWith(newUserDto);
+      expect(authService.getToken).toHaveBeenCalledWith(3);
+    });
   });
 });
--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,18 +1,110 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
+import { UsersRepository } from './users.repository';
+import { Users } from './entity/users.entity';
+import { UserType } from './enum/userType.enum';
+import { UsersDto } from './dto/users.dto';
+
+export class MockUsersRepository {
+  readonly mockUser: Users = {
+    userId: 1,
+    userType: UserType.Instructor,
+    email: 'test@example.com',
+    provider: 'kakao',
+    name: '홍길동',
+    birth: '1993.02.13',
+    phoneNumber: '010-1234-1234',
+    profileImage: 'profileImage.png',
+    userCreatedAt: new Date(),
+    userUpdatedAt: new Date(),
+  };
+}
+
+const mockUser = new MockUsersRepository().mockUser;
 
 describe('UsersService', () => {
-  let service: UsersService;
+  let usersService: UsersService;
+  let usersRepository: UsersRepository;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [
+        UsersService,
+        {
+          provide: UsersRepository,
+          useValue: { findUserByEmail: jest.fn(), createUser: jest.fn() },
+        },
+      ],
     }).compile();
 
-    service = module.get<UsersService>(UsersService);
+    usersService = module.get<UsersService>(UsersService);
+    usersRepository = module.get<UsersRepository>(UsersRepository);
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(usersService).toBeDefined();
+  });
+
+  describe('findUserByEmail', () => {
+    it('email에 해당하는 user를 return', async () => {
+      const email = 'test@example.com';
+      const provider = 'kakao';
+      jest
+        .spyOn(usersRepository, 'findUserByEmail')
+        .mockResolvedValue(mockUser);
+
+      const result = await usersService.findUserByEmail(email, provider);
+      expect(result).toEqual(mockUser);
+      expect(usersRepository.findUserByEmail).toHaveBeenCalledWith(
+        email,
+        provider,
+      );
+    });
+
+    it('찾지 못할 경우 undefined를 return', async () => {
+      const email = 'nonexistent@example.com';
+      const provider = 'google';
+
+      jest
+        .spyOn(usersRepository, 'findUserByEmail')
+        .mockResolvedValue(undefined);
+
+      const result = await usersService.findUserByEmail(email, provider);
+      expect(result).toBeUndefined();
+      expect(usersRepository.findUserByEmail).toHaveBeenCalledWith(
+        email,
+        provider,
+      );
+    });
+  });
+
+  describe('createUser', () => {
+    it('새로운 user를 생성 후에 user를 return', async () => {
+      const newUserDto: UsersDto = {
+        userType: UserType.Customer,
+        email: 'new@example.com',
+        provider: 'kakao',
+        name: '홍길순',
+        profileImage: 'prof.png',
+        phoneNumber: '010-1616-1616',
+        birth: '1995.09.13',
+      };
+      const newUser = new Users();
+      newUser.userId = 2;
+      newUser.userType = newUserDto.userType;
+      newUser.email = newUserDto.email;
+      newUser.provider = newUserDto.provider;
+      newUser.name = newUserDto.name;
+      newUser.profileImage = newUserDto.profileImage;
+      newUser.birth = newUserDto.birth;
+      newUser.userCreatedAt = new Date();
+      newUser.userUpdatedAt = new Date();
+
+      jest.spyOn(usersRepository, 'createUser').mockResolvedValue(newUser);
+
+      const result = await usersService.createUser(newUserDto);
+      expect(result).toEqual(newUser);
+      expect(usersRepository.createUser).toHaveBeenCalledWith(newUserDto);
+    });
   });
 });
