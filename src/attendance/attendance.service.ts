26e2aa38e892905daf27aa6c7fdfa408a712a712
YOONSOO7429 <125416958+YOONSOO7429@users.noreply.github.com>
2024-08-19 15:15:55 +0900

    feat: 출결 관리 추가

--- src/attendance/attendance.service.ts
+++ src/attendance/attendance.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import { AttendanceRepository } from './attendance.repository';
 import { AttendanceDto } from './dto/attendance.dto';
 import { Attendance } from './entity/attendance.entity';
@@ -9,13 +9,21 @@ export class AttendanceService {
 
   /* 출석 체크 */
   async createAttendance(attendanceDto: AttendanceDto): Promise<void> {
-    await this.attendanceRepository.createAttendance(attendanceDto);
+    const attendance =
+      await this.attendanceRepository.createAttendance(attendanceDto);
+    if (!attendance) {
+      throw new InternalServerErrorException('출결 체크 실패');
+    }
   }
 
   /* 출석 취소 */
   async deleteAttendance(attendanceDto: AttendanceDto): Promise<void> {
     const deleteResult =
       await this.attendanceRepository.deleteAttendance(attendanceDto);
+
+    if (deleteResult.affected === 0) {
+      throw new InternalServerErrorException('출결 취소 실패');
+    }
   }
 
   /* 출석 횟수 조회*/
